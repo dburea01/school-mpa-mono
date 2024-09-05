054909652912568436afdd2ed7281e88060d3017
domi <dburea01@gmail.com>
2024-09-05 11:45:12 +0200

    essai lighthouse

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="template admin bootstrap">
-    <meta name="csrf-token" content="{{ csrf_token() }}"> 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -74,7 +74,7 @@
                                     <hr class="dropdown-divider">
                                 </li>
                                 <li>
-                                    <a class="dropdown-item disabled" aria-disabled="true">{{ $currentPeriod->name }}</a>
+                                    <a href="#" class="dropdown-item disabled" aria-disabled="true">{{ $currentPeriod->name }}</a>
                                 </li>
                                 @can('viewAny', App\models\Classroom::class)
                                 <li>
